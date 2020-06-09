26cbe1e96cc1b846b5c7380dfc672881613536de
Shawn-newteng <844699335@qq.com>
2020-06-09 10:57:13 +0800

    feat: 完善权限菜单

---
+++ app/Listeners/BuildingUserMenus.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\AdminMenu;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
+
+class BuildingUserMenus
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function handle(BuildingMenu $event)
+    {
+        $menu = new AdminMenu;
+        $menus = $menu
+            ->with('subMenus')
+            ->where('parent_id', 0)
+            ->orderBy('order')
+            ->get();
+
+        $active = function ($uri) {
+            if ($uri) {
+                $uri = url()->route($uri, [], false);
+                return  ['regex:@^' . substr($uri, 1) . '/[0-9]+/.*$@'];
+            }
+            return '';
+        };
+
+        $func = function ($menus) use (&$func, $event, $active) {
+            foreach ($menus as $menu) {
+                $menu_info = [
+                    'key' => $menu->id,
+                    'text' => $menu->title,
+                    'url' => $menu->permission ? route($menu->permission) : $menu->uri,
+                    'icon' => 'nav-icon fas ' . $menu->icon,
+                    'active' => $active($menu->permission),
+                ];
+
+                if ($menu->permission) {
+                    $menu_info['can'] = $menu->permission;
+                }
+
+                if ($menu->parent) {
+                    $event->menu->addin($menu->parent_id, $menu_info);
+                } else {
+                    $event->menu->add($menu_info);
+                }
+
+                if (count($menu->subMenus)) {
+                    $func($menu->subMenus);
+                }
+            }
+        };
+
+        $func($menus);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,53 +21,8 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 
-    public function boot(Dispatcher $events)
+    public function boot()
     {
-        //监听菜单构造事件、构造用户权限菜单
-        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
-            $menu = new AdminMenu;
-            $menus = $menu
-                ->with('subMenus')
-                ->where('parent_id', 0)
-                ->orderBy('order')
-                ->get();
-
-            $active = function ($uri) {
-                if ($uri) {
-                    $uri = url()->route($uri, [], false);
-                    return  ['regex:@^' . substr($uri, 1) . '/[0-9]+/.*$@'];
-                }
-                return '';
-            };
-
-            $func = function ($menus) use (&$func, $event, $active) {
-                foreach ($menus as $menu) {
-                    $event->menu->addin($menu->parent_id, [
-                        'text' => $menu->title,
-                        'url' => $menu->permission ? route($menu->permission) : '',
-                        'icon' => 'nav-icon fas ' . $menu->icon,
-                        'active' => $active($menu->permission),
-                    ]);
-
-                    if (count($menu->subMenus)) {
-                        $func($menu->subMenus);
-                    }
-                }
-            };
-
-            foreach ($menus as $menu) {
-                $event->menu->add([
-                    'key' => $menu->id,
-                    'text' => $menu->title,
-                    'url' => $menu->permission ? route($menu->permission) : '',
-                    'icon' => 'nav-icon fas ' . $menu->icon,
-                    'active' => $active($menu->permission),
-                ]);
-
-                if (count($menu->subMenus)) {
-                    $func($menu->subMenus);
-                }
-            }
-        });
+        //
     }
 }
