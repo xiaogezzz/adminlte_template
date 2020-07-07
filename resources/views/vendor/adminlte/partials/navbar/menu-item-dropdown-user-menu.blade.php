44c1b6a7194c7ea534a27b1ac1591a0e0c285e60
Shawn-newteng <844699335@qq.com>
2020-07-07 16:04:33 +0800

    fix: upgrade jeroennoten/laravel-adminlte to v3.4.2 and complete composer.json

--- resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
@@ -23,7 +23,7 @@
                  alt="{{ Auth::user()->name }}">
         @endif
         <span @if(config('adminlte.usermenu_image')) class="d-none d-md-inline" @endif>
-            {{ Auth::user()->nickname }}
+            {{ Auth::user()->name }}
         </span>
     </a>
 
