ac34c90a09301c2afd2f4efe7d7ff8804031ab98
Shawn-newteng <844699335@qq.com>
2020-08-11 18:02:00 +0800

    update jeroennoten/laravel-adminlte to v3.4.4

--- config/adminlte.php
+++ config/adminlte.php
@@ -230,87 +230,7 @@ return [
     |
     */
 
-    'menu' => [
-//        [
-//            'text' => 'search',
-//            'search' => true,
-//            'topnav' => true,
-//        ],
-//        [
-//            'text' => 'blog',
-//            'url'  => 'admin/blog',
-//            'can'  => 'manage-blog',
-//        ],
-//        [
-//            'text'        => 'pages',
-//            'url'         => 'admin/pages',
-//            'icon'        => 'far fa-fw fa-file',
-//            'label'       => 4,
-//            'label_color' => 'success',
-//        ],
-//        ['header' => 'account_settings'],
-//        [
-//            'text' => 'profile',
-//            'url'  => 'admin/settings',
-//            'icon' => 'fas fa-fw fa-user',
-//        ],
-//        [
-//            'text' => 'change_password',
-//            'url'  => 'admin/settings',
-//            'icon' => 'fas fa-fw fa-lock',
-//        ],
-//        [
-//            'text'    => 'multilevel',
-//            'icon'    => 'fas fa-fw fa-share',
-//            'submenu' => [
-//                [
-//                    'text' => 'level_one',
-//                    'url'  => '#',
-//                ],
-//                [
-//                    'text'    => 'level_one',
-//                    'url'     => '#',
-//                    'submenu' => [
-//                        [
-//                            'text' => 'level_two',
-//                            'url'  => '#',
-//                        ],
-//                        [
-//                            'text'    => 'level_two',
-//                            'url'     => '#',
-//                            'submenu' => [
-//                                [
-//                                    'text' => 'level_three',
-//                                    'url'  => '#',
-//                                ],
-//                                [
-//                                    'text' => 'level_three',
-//                                    'url'  => '#',
-//                                ],
-//                            ],
-//                        ],
-//                    ],
-//                ],
-//                [
-//                    'text' => 'level_one',
-//                    'url'  => '#',
-//                ],
-//            ],
-//        ],
-//        ['header' => 'labels'],
-//        [
-//            'text'       => 'important',
-//            'icon_color' => 'red',
-//        ],
-//        [
-//            'text'       => 'warning',
-//            'icon_color' => 'yellow',
-//        ],
-//        [
-//            'text'       => 'information',
-//            'icon_color' => 'cyan',
-//        ],
-    ],
+    'menu' => [],
 
     /*
     |--------------------------------------------------------------------------
@@ -347,8 +267,7 @@ return [
     */
 
     'plugins' => [
-        [
-            'name' => 'Datatables',
+        'Datatables' => [
             'active' => false,
             'files' => [
                 [
@@ -368,8 +287,7 @@ return [
                 ],
             ],
         ],
-        [
-            'name' => 'Select2',
+        'Select2' => [
             'active' => true,
             'files' => [
                 [
@@ -382,10 +300,14 @@ return [
                     'asset' => true,
                     'location' => 'vendor/select2/css/select2.min.css',
                 ],
+                [
+                    'type' => 'css',
+                    'asset' => true,
+                    'location' => 'vendor/select2-bootstrap4-theme/select2-bootstrap4.min.css',
+                ],
             ],
         ],
-        [
-            'name' => 'Chartjs',
+        'Chartjs' => [
             'active' => false,
             'files' => [
                 [
@@ -395,10 +317,14 @@ return [
                 ],
             ],
         ],
-        [
-            'name' => 'Sweetalert2',
+        'Sweetalert2' => [
             'active' => true,
             'files' => [
+                [
+                    'type' => 'css',
+                    'asset' => true,
+                    'location' => 'vendor/sweetalert2/sweetalert2.min.css',
+                ],
                 [
                     'type' => 'css',
                     'asset' => true,
@@ -411,8 +337,7 @@ return [
                 ],
             ],
         ],
-        [
-            'name' => 'Pace',
+        'Pace' => [
             'active' => true,
             'files' => [
                 [
@@ -427,8 +352,7 @@ return [
                 ],
             ],
         ],
-        [
-            'name' => 'jQuery Form',
+        'jQuery Form' => [
             'active' => true,
             'files' => [
                 [
@@ -438,8 +362,7 @@ return [
                 ],
             ],
         ],
-        [
-            'name' => 'jQuery Validate',
+        'jQuery Validate' => [
             'active' => true,
             'files' => [
                 [
@@ -449,8 +372,7 @@ return [
                 ],
             ],
         ],
-        [
-            'name' => 'axios',
+        'axios' => [
             'active' => true,
             'files' => [
                 [
@@ -460,8 +382,7 @@ return [
                 ],
             ],
         ],
-        [
-            'name' => 'Bootstrap Duallistbox',
+        'Bootstrap Duallistbox' => [
             'active' => false,
             'files' => [
                 [
